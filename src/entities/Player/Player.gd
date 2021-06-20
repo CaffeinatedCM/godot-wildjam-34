475b1980e0c25b59a22ee6db7ea2240dca741af8
Joshua Letcher <caffeinatedcm@gmail.com>
2021-06-19 23:56:12 -0400

    add player hurtbox and stuff

--- src/entities/Player/Player.gd
+++ src/entities/Player/Player.gd
@@ -2,6 +2,14 @@ extends KinematicBody2D
 
 class_name Player
 
+export var Health = 10
+
 func _ready():
     $AnimationTree.active = true
     $GunFlashAnimationTree.active = true
+
+
+func _on_Hurtbox_area_entered(area):
+    Health -= area.DAMAGE
+    print(Health)
+    pass # Replace with function body.
